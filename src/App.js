5e93e9e15642fb7cf523c4b1fd5ecb944b6296f1
Kerstin Wiessmann <kwiessmann@gmail.com>
2025-08-10 19:30:07 +0200

    Added Search Engine and footer

--- src/App.js
+++ src/App.js
@@ -6,9 +6,36 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello from React</h1>
+        <h1>Weather App</h1>
         <Weather city="New York" />
       </header>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/KerstinWieseline/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Kerstin Wiessmann
+        </a>
+        , open-sourced on{" "}
+        <a
+          href="https://github.com/KerstinWieseline/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://weather-react-kerstin.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+        .
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-export default function Weather(props) {
-    function handleResponse(response) {
-        alert(`The weather in New York is ${response.data.temperature.current}°C`);
-    }
+export default function WeatherSearch() {
+  let [city, setCity] = useState("");
+  let [loaded, setLoaded] = useState(false);
+  let [weather, setWeather] = useState({});
+
+  function showTemperature(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.temperature.current,
+      description: response.data.condition.description,
+      wind: response.data.wind.speed,
+      humidity: response.data.temperature.humidity,
+      icon: response.data.condition.icon_url,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
     let apiKey = "0c43aab85t9fo6a6712276a1886b3109";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
-    return <h2>Hello from Weather</h2>;
+    let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(url).then(showTemperature);
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Enter a city..."
+        onChange={updateCity}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {Math.round(weather.humidity)}%</li>
+          <li>Wind: {Math.round(weather.wind)}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
